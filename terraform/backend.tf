ffc4ff52d96f749b23b596db882ad4e32f8687a2
Your Name <you@example.com>
2026-06-09 10:52:03 +1000

    fix all pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "rnd-terraform-ansible-20260609"
-    key     = "terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket         = "rnd-terraform-ansible-20260609"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
   }
 }
